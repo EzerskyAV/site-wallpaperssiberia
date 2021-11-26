38da45ba2a00df06d082fdf0e746e17d01a3ace6
EzerskyAV <87753217+EzerskyAV@users.noreply.github.com>
2021-11-26 09:22:35 +0700

    https://github.com/EzerskyAV/site-wallpaperssiberia

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to our instagramm <a href="https://www.instagram.com/wallpaperssiberia/">WallpapersSiberia</a>
         </h1>
 
         <p className={styles.description}>
@@ -22,33 +22,25 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://www.instagram.com/p/CTZ1R-PDQvT/" className={styles.card}>
+            <h2>CAT &rarr;</h2>
+            <p>Cat photos</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://www.instagram.com/p/CWaGJXYoWny/" className={styles.card}>
+            <h2>Water &rarr;</h2>
+            <p>Ob river</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://www.instagram.com/p/CWVo2LDorpf/"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h2>Nature &rarr;</h2>
+            <p>Siberian nature</p>
           </a>
+
+          
         </div>
       </main>
 
